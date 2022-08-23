f95dd01b01246a453e5b2c45576acfe4775358c1
Vlaznev Victor <v-vlaznev@mail.ru>
2022-08-23 16:12:03 +0300

    Create task2 in seminar2

---
+++ seminar2/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру (слева направо) заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 6
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int thirdDigitNumber = int.Parse(Console.ReadLine());
+int thirdDigit = thirdDigitNumber / 100 % 10;
+if (thirdDigit == 0)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(thirdDigit);
+}
