1d37d302820a38bee2a48da40c3641126e8035df
Vlaznev Victor <v-vlaznev@mail.ru>
2022-08-30 17:53:38 +0300

    Add task2 in seminar4

---
+++ seminar4/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumOfDigits(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр числа {number} = {SumOfDigits(number)}");
