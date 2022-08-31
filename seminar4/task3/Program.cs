e08d4df952ba42f4be9e74095f2aca69c89350db
Vlaznev Victor <v-vlaznev@mail.ru>
2022-08-31 10:19:00 +0300

    Add task3 in seminar4

---
+++ seminar4/task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов, заполненных случайнми числами из [a, b) и выводит их на экран.
+// 5, 0, 20 -> [1, 2, 5, 7, 19]
+// 3, 1, 35 -> [6, 1, 33]
+
+// функция создания массива по заданной длине и интервале
+int[] CreateArray(int len, int min, int max)
+{
+    int[] array = new int[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+
+    return array;
+}
+
+// функция печати массива
+void PrintArray(int[] arr)
+{
+    Console.WriteLine($"[{String.Join(", ", arr)}]");
+}
+
+PrintArray(CreateArray(5, 0, 20));
+PrintArray(CreateArray(3, 1, 35));
