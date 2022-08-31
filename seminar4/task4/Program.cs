71d0f780edd72728f24edd6ea99e84fd94487187
Vlaznev Victor <v-vlaznev@mail.ru>
2022-08-31 14:04:19 +0300

    Add task4 in seminar4

---
+++ seminar4/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача “со звездочкой”: Напишите функцию, которая принимает одно число - высоту елочки и рисует ее в консоли звездочками.
+
+void MadeChristmasTree(int num)
+{
+    for (int i = 1; i <= num; i++)
+            {
+                Console.Write(new string(' ', num - i));
+                Console.Write(new string('*', 2*i - 1));
+                Console.WriteLine();
+            }
+}
+
+Console.Write("Введите высоту ёлочки: ");
+int hight = int.Parse(Console.ReadLine());
+MadeChristmasTree(hight);
