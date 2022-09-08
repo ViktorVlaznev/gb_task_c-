b3b3f6cd13de282699aad01456d5401380c49e6a
Елизавета <elizaveta@Air-Elizaveta.Dlink>
2022-09-08 16:05:34 +0300

    Add task1 in seminar6

---
+++ seminar6/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void CountPositivNum(int quantity)
+{
+    int count = 0;
+    for (int i = 0; i < quantity; i++)
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number > 0) count++;
+    }
+    Console.WriteLine($"Кол-во положительных чисел: {count}");
+}
+Console.Write("Введите кол-во чисел: ");
+int quant = int.Parse(Console.ReadLine());
+CountPositivNum(quant);
